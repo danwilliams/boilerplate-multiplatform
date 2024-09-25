4ef7663c82d5e85ca0d927b987055028096b1096
Dan Williams <dan@dotfive.co.uk>
2024-09-25 06:48:43 +0100

    Added iOS application foundation
      - Added iOS application foundation using default app template from
        Xcode.

---
+++ ios/Boilerplate/BoilerplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoilerplateApp.swift
+//  Boilerplate
+//
+//  Created by Dan Williams on 24/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BoilerplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/Boilerplate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Boilerplate
+//
+//  Created by Dan Williams on 24/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
