e42703f8015139d6b06ab44d239223a99a264520
Dan Williams <dan@dotfive.co.uk>
2024-09-25 06:42:02 +0100

    Added MacOS application foundation
      - Added MacOS application foundation using default app template from
        Xcode.

---
+++ macos/BoilerplateTests/BoilerplateTests.swift
@@ -0,0 +1,17 @@
+//
+//  BoilerplateTests.swift
+//  BoilerplateTests
+//
+//  Created by Dan Williams on 24/09/2024.
+//
+
+import Testing
+@testable import Boilerplate
+
+struct BoilerplateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
